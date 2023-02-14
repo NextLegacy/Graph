d77d88ee5d49cd21fcbcbd58e81e8a3168ec330a
NextLegacy <mcserezli@gmail.com>
2023-02-14 21:36:25 +0100

    began implementing Graph class

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,6 +1,45 @@
 package graph;
 
-public class Graph 
+import static graph.utils.ArrayUtils.*;
+
+public class Graph<T>
 {
-    
+    private Node<T>[] nodes;
+    private Edge<T>[] edges;
+
+    public void addNode(Node<T> node, double weight)
+    {
+        if (contains(nodes, node))
+            throw new IllegalArgumentException("Node " + node + " is already a node of " + this + ".");
+
+        nodes = push(nodes, node);
+    }
+
+    public void removeNode(Node<T> node)
+    {
+        nodes = remove(nodes, node);
+        
+        final Edge<T>[] edgesToNode = filter(edges, edge -> edge.to() == node || edge.from() == node);
+
+        edges = remove(edges, edgesToNode);
+    }
+
+    public void addEdge(Edge<T> edge)
+    {
+        if (contains(edges, edge))
+            throw new IllegalArgumentException("Edge " + edge + " is already an edge of " + this + ".");
+
+        if (!contains(nodes, edge.from()))
+            throw new IllegalArgumentException("Node " + edge.from() + " is not a node of " + this + ".");
+
+        if (!contains(nodes, edge.to()))
+            throw new IllegalArgumentException("Node " + edge.to() + " is not a node of " + this + ".");
+
+        edges = push(edges, edge);
+    }
+
+    public void removeEdge(Edge<T> edge)
+    {
+        edges = remove(edges, edge);
+    }
 }
