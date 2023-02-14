848579043aa871a0f7fefbb8473eef3441e62e71
NextLegacy <mcserezli@gmail.com>
2023-02-14 21:36:15 +0100

    began implementing Node class

--- src/graph/Node.java
+++ src/graph/Node.java
@@ -1,6 +1,16 @@
 package graph;
 
-public class Node 
+public class Node<T>
 {
-    
+    boolean visited;
+    T data;
+
+    public Node(T data)
+    {
+        this.data = data;
+    }
+
+    public T data() { return data; }
+
+    public void setData(T data) { this.data = data; }
 }
